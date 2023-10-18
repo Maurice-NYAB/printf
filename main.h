8b82f188c7e697fbaa8e5a18e1d2e13b7030c889
Maurice-NYAB <maurice.adjeibaah@hotmail.com>
2023-10-17 22:15:59 -0700

    unprintable characters_task 5

--- main.h
+++ main.h
@@ -21,6 +21,8 @@ int print_o(va_list arg);
 int print_hex(va_list arg);
 int print_HEX(va_list arg);
 int print_pointer(va_list arg);
+int print_s_string(va_list arg_list);
 int print_hex_extra(unsigned long int num);
 int print_revs(va_list arg);
+int print_HEX_upper(unsigned int num);
 #endif
---
+++ print_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_s_string - a function that prints non printable characters
+ * @arg_list: argument
+ * Return: number of characters printed
+ */
+
+int print_s_string(va_list arg_list)
+{
+	char *s;
+	int i, len = 0;
+	int value;
+
+	s = va_arg(arg_list, char *);
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i] !='\0'; i++)
+	{
+		if (s[i] < 32 || s[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			len = len + 2;
+			value = s[i];
+			if (value < 16)
+			{
+				_putchar('0');
+				len++;
+			}
+			len = len + print_HEX_upper(value);
+		}
+		else
+		{
+			_putchar(s[i]);
+			len++;
+		}
+	}
+	return (len);
+}
